1fbd2c695b282b2f45043b096c55d9c8a4dccf13
xiaogua654 <13744374+xiaogua654@user.noreply.gitee.com>
2024-12-30 20:13:29 +0800

    新增员工代码开发完善

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,19 +3,19 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +23,7 @@ import java.util.Map;
 /**
  * 员工管理
  */
+@Api(tags = "用户管理" )
 @RestController
 @RequestMapping("/admin/employee")
 @Slf4j
@@ -81,9 +82,32 @@ public class EmployeeController {
     @PostMapping("/save")
     @ApiOperation(value = "新增员工")
     public Result  save(@RequestBody EmployeeDTO employeedto) {
+        log.info("新增员工：{}", employeedto);
+        employeeService.save(employeedto);
+        return Result.success();
+    }
+    /**
+     * 启用、禁用员工账号
+     * @param employeedto
+     * @return
+     */
+    @PostMapping("/status")
+    @ApiOperation(value = "启用、禁用员工账号")
+    public Result  status(@RequestBody EmployeeDTO employeedto) {
         log.info("新增员工 ：{}", employeedto);
         employeeService.save(employeedto);
         return Result.success();
     }
-
+    /**
+     * 员工分页查询
+     * @param  employeePageQueryDTO
+     * @return 列表
+     */
+    @GetMapping("/page")
+    @ApiOperation(value = "员工分页查询")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO) {
+        log.info("员工分页查询 ：{}", employeePageQueryDTO);
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
+    }
 }
